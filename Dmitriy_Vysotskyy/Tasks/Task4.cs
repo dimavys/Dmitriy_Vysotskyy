141ed6b1b9c20d0c27e06d0d6ae88fb3277a8359
Dmitriy <dimashkavysotskyy@gmail.com>
2022-12-13 13:24:09 +0200

    Task4 final commit

---
+++ Dmitriy_Vysotskyy/Tasks/Task4.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Dmitriy_Vysotskyy.Tasks
+{
+	public static class Task4
+	{
+        public static int GetTargetPair(int[] array, int target)
+        {
+            int counter = 0;
+
+            for (var i = 0; i < array.Length - 1; i++)
+                for (var j = i + 1; j < array.Length; j++)
+                    if ((array[i] + array[j]) == target)
+                        counter++;
+
+            return counter;
+        }
+    }
+}
+
