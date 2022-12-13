12ebdd259a52d49c20ae4f04910d783eba76f327
Dmitriy <dimashkavysotskyy@gmail.com>
2022-12-13 13:17:21 +0200

    Task3 final commit

---
+++ Dmitriy_Vysotskyy/Tasks/Task3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Dmitriy_Vysotskyy.Tasks
+{
+	public static class Task3
+	{
+        public static int GetDigitalRoot(int n)
+        {
+            int root = 0;
+            while (n > 0 || root > 9)
+            {
+                if (n == 0)
+                {
+                    n = root;
+                    root = 0;
+                }
+
+                root += n % 10;
+                n /= 10;
+            }
+            return root;
+        }
+    }
+}
+
---
+++ Dmitriy_Vysotskyy/Tests/Test3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Dmitriy_Vysotskyy.Tasks;
+using NUnit.Framework;
+
+namespace Dmitriy_Vysotskyy.Tests
+{
+	public class Test3
+	{
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void TestA()
+        {
+            Assert.AreEqual(Task3.GetDigitalRoot(16), 7);
+        }
+
+        [Test]
+        public void TestB()
+        {
+            Assert.AreEqual(Task3.GetDigitalRoot(942), 6);
+        }
+
+        [Test]
+        public void TestC()
+        {
+            Assert.AreEqual(Task3.GetDigitalRoot(132189), 6);
+        }
+
+        [Test]
+        public void TestD()
+        {
+            Assert.AreEqual(Task3.GetDigitalRoot(493193), 2);
+        }
+    }
+}
+
