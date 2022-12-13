04cf2010c6673214ae1898fea47a6889c45b2594
Dmitriy <dimashkavysotskyy@gmail.com>
2022-12-13 12:54:49 +0200

    Task 1 init

---
+++ Dmitriy_Vysotskyy/Tasks/Task1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dmitriy_Vysotskyy.Tasks
+{
+	public static class Task1
+	{
+       public static List<object> GetIntegersFromList(List<object> inputList)
+       {
+            foreach (var item in inputList)
+                if (item.GetType() != typeof(System.Int32))
+                    inputList.Remove(item);
+
+            return inputList;
+       }
+    }
+}
+
---
+++ Dmitriy_Vysotskyy/Tests/Test1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Dmitriy_Vysotskyy.Tasks;
+using NUnit.Framework;
+
+namespace Dmitriy_Vysotskyy.Tests
+{
+	public class Test1
+	{
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test()
+        {
+            
+            Assert.AreEqual(Task1.GetIntegersFromList(new List<object>() { 1, 2, 'a', 'b' }), new List<object>() {1, 2});
+        }
+
+    }
+}
+
