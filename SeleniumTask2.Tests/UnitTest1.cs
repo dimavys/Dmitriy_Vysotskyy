72dfa224753954dac197761581b66dff81baa12e
Dmitriy <dimashkavysotskyy@gmail.com>
2022-12-21 19:44:51 +0200

    add: tst test fix

--- SeleniumTask2.Tests/UnitTest1.cs
+++ SeleniumTask2.Tests/UnitTest1.cs
@@ -15,28 +15,26 @@ public class Tests
     [Test, Order(1)]
     public async Task TestUpload()
     {
-        var exists = File.Exists("Usings.cs");
-
-        var result = await _restClient.UploadFile("files/file.txt");
+        var result = await _restClient.UploadFile("/Users/mac/Desktop/file.txt");
 
         Assert.AreEqual(true, result.is_downloadable);
     }
 
-    //[Test, Order(2)]
-    //public async Task TestMetadata()
-    //{
-    //    var result = await _restClient.GetFileMetadata("/file.txt");
+    [Test, Order(2)]
+    public async Task TestMetadata()
+    {
+        var result = await _restClient.GetFileMetadata("/file.txt");
 
-    //    Assert.AreEqual("file.txt", result.name);
-    //}
+        Assert.AreEqual("file.txt", result.name);
+    }
 
-    //[Test, Order(3)]
-    //public async Task TestDelete()
-    //{
-    //    var result = await _restClient.DeleteFile("/file.txt");
+    [Test, Order(3)]
+    public async Task TestDelete()
+    {
+        var result = await _restClient.DeleteFile("/file.txt");
 
-    //    Assert.AreEqual(false, result.is_downloadable);
-    //}
+        Assert.AreEqual(false, result.is_downloadable);
+    }
 
     [OneTimeTearDown]
     public void TearDown()
