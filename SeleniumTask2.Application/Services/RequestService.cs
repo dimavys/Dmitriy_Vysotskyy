219e4558521c0fb7719fc1ab67bf64e4eab85896
Dmitriy <dimashkavysotskyy@gmail.com>
2022-12-21 20:51:49 +0200

    add: service layer

---
+++ SeleniumTask2.Application/Services/RequestService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using RestSharp;
+using SeleniumTask2.Application.Constants;
+
+namespace SeleniumTask2.Application.Services
+{
+	public static class RequestService
+	{
+        public static RestRequest BuildGetRequest(string filePath)
+		{
+            var request = new RequestBuilder.RequestBuilder()
+               .SetUrl(RouteConstants.GetMetaDataUrl)
+               .SetHeader("Content-Type", "application/json")
+               .SetBody<object>(new { include_deleted = false, include_has_explicit_shared_members = false, include_media_info = false, path = filePath })
+               .Build();
+
+            return request;
+        }
+
+        public static RestRequest BuildPostRequest(string localFilePath)
+		{
+            var request = new RequestBuilder.RequestBuilder()
+                .SetUrl(RouteConstants.UploadFileUrl)
+                .SetHeader("Dropbox-API-Arg", "{\"path\":\"/file.txt\"}")
+                .SetHeader("Content-Type", "application/octet-stream")
+                .SetFile(localFilePath)
+                .Build();
+
+            return request;
+        }
+
+        public static RestRequest BuildDeleteRequest(string filePath)
+		{
+            var request = new RequestBuilder.RequestBuilder()
+                .SetUrl(RouteConstants.DeleteFileUrl)
+                .SetHeader("Content-Type", "application/json")
+                .SetBody<object>(new { path = filePath })
+                .Build();
+
+            return request;
+        }
+
+    }
+}
+
---
+++ SeleniumTask2.Tests/UploadScenario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using SeleniumTask2.Application.RestApiClient;
+
+namespace SeleniumTask2.Tests
+{
+    public class UploadScenario
+    {
+        private RestApiClient _restClient;
+
+        private readonly string _localFilePath = "/Users/mac/Desktop/file.txt";
+
+        [SetUp]
+        public void Setup()
+        {
+            _restClient = new RestApiClient();
+        }
+
+        [Test]
+        public async Task UploadFile_TxtFile_ShouldUploadSuccessfully()
+        {
+            var result = await _restClient.UploadFile(_localFilePath);
+
+            Assert.AreEqual(true, result.is_downloadable);
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            await _restClient.DeleteFile("/file.txt");
+
+            _restClient.Dispose();
+        }
+    }
+}
+
