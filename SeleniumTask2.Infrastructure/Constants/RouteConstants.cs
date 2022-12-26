53a3ae5a566a077f177dcdddb1f5cc5beead5014
Dmitriy <dimashkavysotskyy@gmail.com>
2022-12-26 22:41:57 +0200

    token tmp change

--- SeleniumTask2.Infrastructure/Constants/RouteConstants.cs
+++ SeleniumTask2.Infrastructure/Constants/RouteConstants.cs
@@ -12,7 +12,6 @@ namespace SeleniumTask2.Application.Constants
 		public static readonly string UploadFileUrl = "https://content.dropboxapi.com/2/files/upload";
 
         public static readonly string TokenType = "Bearer";
-
 		public static readonly string GeneratedToken = "sl.BVsvJpN4lqNo5MMfo_sDdB9K2iRYZVm7SCB-Tf_yQDil9m0tWGXDWSPqiLO_-9_eYDoZlfV059e94Z72L_Opr7bqDmnVqTOTXBbbfMacKaZtRUGqxqRMnRlscI-vY5B9oFBj5K8QjEjB";
     }
 }
